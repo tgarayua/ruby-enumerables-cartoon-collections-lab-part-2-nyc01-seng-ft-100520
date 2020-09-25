decc1c28c36ec5a856e85fb66730b7daf2f2c834
Thomas Garayua <tgarayua@gmail.com>
2020-09-25 21:32:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,3 +13,16 @@ def square_array(array)
   new_array
 end
 
+def summon_captain_planet(planeteer_calls)
+  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
+  # Return a new array of the results
+  new_array = []
+  index = 0 
+  
+  while index < planeteer_calls.length do
+    new_array << planeteer_calls[index].capitalize + "!"
+    index += 1 
+  end
+  new_array
+end
+
